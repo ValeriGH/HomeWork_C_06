fb9eb0e1210ad38195bee39459dd28bdf9458f81
Valeria <valeria_korzun@mail.ru>
2022-12-15 00:56:17 +0300

    Количество положительных чисел

---
+++ Task_41/Program.cs
@@ -0,0 +1,15 @@
+﻿// Пользователь вводит m чисел. 
+// Посчитать количество чисел > 0 
+
+Console.Clear();
+Console.Write("Сколько чисел будете вводить? ");
+int m = int.Parse(Console.ReadLine());
+int[] numbers = new int[m];
+int count = 0;
+for (int i = 0; i < m; i++)
+{
+    Console.Write($"Введите {i + 1} число: ");
+    numbers[i] = int.Parse(Console.ReadLine());
+    if (numbers[i] > 0) count++;
+}
+Console.WriteLine($"Вы ввели чисел больше нуля: {count}");
